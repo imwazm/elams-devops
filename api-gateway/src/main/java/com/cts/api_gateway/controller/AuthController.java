ad80ee48bed00b7da652353bc24a664dd3c70694
darkwizz-cts <mohamedakram.mohamednazir@cognizant.com>
2025-06-15 20:08:35 +0530

    Fix request mapping

--- api-gateway/src/main/java/com/cts/api_gateway/controller/AuthController.java
+++ api-gateway/src/main/java/com/cts/api_gateway/controller/AuthController.java
@@ -11,14 +11,12 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
-@RestController("auth")
+@RestController
+@RequestMapping("auth")
 public class AuthController {
 
     @Autowired
